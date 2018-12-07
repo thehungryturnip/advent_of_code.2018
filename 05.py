570534e85b919361b133423e86d66e406f54b2ba
The Hungry Turnip <thehungryturnip@gmail.com>
2018-12-07 17:56:56 -0500

    completed 05b

--- 05.py
+++ 05.py
@@ -21,15 +21,11 @@ if __name__ == '__main__':
                 string += str(n.data)
             return string
 
-    filename = sys.argv[1]
-    print(f'file: {filename}')
-
-    head = None
-    tail = None
-    with open(filename, 'r') as file_:
-        c = file_.read(1)
-        while c:
-            if not c.isspace():
+    def create_list(data, exclude=None):
+        head = None
+        tail = None
+        for c in line:
+            if not c.lower() == exclude:
                 n = Node(c)
                 if not head:
                     head = n
@@ -38,20 +34,52 @@ if __name__ == '__main__':
                 tail.next_ = n
                 tail.next_.prev = tail
                 tail = n
-            c = file_.read(1)
-    print(head)
-    
+        return head, tail
+
+    def react_polymer(head):
+        cursor = head
+        while cursor:
+            if cursor.next_ and cursor.data.swapcase() == cursor.next_.data:
+                if cursor is head:
+                    cursor = cursor.next_.next_
+                    head = cursor
+                else:
+                    cursor = cursor.prev
+                    cursor.next_ = cursor.next_.next_.next_
+                    if cursor.next_:
+                        cursor.next_.prev = cursor
+            else:
+                cursor = cursor.next_
+        return head
+
+    filename = sys.argv[1]
+    print(f'file: {filename}')
+
+    with open(filename, 'r') as file_:
+        line = file_.readline().strip()
+
+    head, tail = create_list(line)
+    head = react_polymer(head)
+    print(f'[05a] {head} is {len(str(head))} long.')
+
+    types = {}
     cursor = head
     while cursor:
-        if cursor.next_ and cursor.data.swapcase() == cursor.next_.data:
-            if cursor is head:
-                cursor = cursor.next_.next_
-                head = cursor
-            else:
-                cursor = cursor.prev
-                cursor.next_ = cursor.next_.next_.next_
-                if cursor.next_:
-                    cursor.next_.prev = cursor
-        else:
-            cursor = cursor.next_
-    print(f'{head} is {len(str(head))} long.')
+        type_ = cursor.data.lower()
+        if type_ not in types:
+            types[type_] = 1
+        cursor = cursor.next_
+
+    removed = None
+    shortest = None
+    for t in types:
+        head, tail = create_list(line, t)
+        head = react_polymer(head)
+        if not shortest:
+            removed = t
+            shortest = head
+            continue
+        if len(str(head)) < len(str(shortest)):
+            shortest = head
+    print(f'[05b] shortest is by removing {removed}' +
+          f' with a length of {len(str(shortest))}.')
