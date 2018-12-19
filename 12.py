cbfa84f168d4b5fa27fc266b96fc658fa95d9ce0
The Hungry Turnip <thehungryturnip@gmail.com>
2018-12-18 21:51:16 -0500

    Completed 12b, but the solution is *ugly*!!!

--- 12.py
+++ 12.py
@@ -39,9 +39,10 @@ if __name__ == '__main__':
                     next_gen += self.rules.get(condition, NO_PLANT)
                 next_gen_lstrip = next_gen.lstrip(NO_PLANT)
                 self.zero += len(next_gen) - len(next_gen_lstrip) - (reach + 1)
-                self.pots = next_gen_lstrip.rstrip(NO_PLANT)
+                next_gen = next_gen_lstrip.rstrip(NO_PLANT)
+                self.pots = next_gen
 
-        def plant_index(self):
+        def garden_index(self):
             total = 0
             for i in range(len(self.pots)):
                 if self.pots[i] == HAS_PLANT:
@@ -71,10 +72,38 @@ if __name__ == '__main__':
                     g.add_rule(condition, result)
             line = f.readline()
 
-    print(f'0:{g}')
-    for i in range(generation_count):
+    curr_index = g.garden_index()
+    print(f'0:{g}:{curr_index}')
+    i = 0
+    while i < generation_count:
         g.progress(reach, 1)
-        print(f'{i + 1}:{g}')
+        prev_index = curr_index
+        curr_index = g.garden_index()
+        print(f'{i + 1}:{g}:{curr_index}:{curr_index - prev_index}')
+        i += 1
 
-    print(f'[12a] the plant index is {g.plant_index()}' +
+    print(f'[12a] the plant index is {g.garden_index()}' +
           f' after {generation_count} generations.')
+
+    # noticed that the general layout of the garden stabilizes with:
+    stable_garden = '#..#.##...#.##....#....#.##...#.....#.##...#.##...#.##...#.##...#..#.##...#.##...#.##...#.##...#.##...#.##...#.##...#.##...#.##...#.##...#.##'
+
+    # thus look for first generation with stable_garden
+    while not g.pots == stable_garden:
+        g.progress(reach, 1)
+        prev_index = curr_index
+        curr_index = g.garden_index()
+        print(f'{i + 1}:{g}:{curr_index}:{curr_index - prev_index}')
+        i += 1
+    
+    # progress 10 more to determine delta and confirm pattern
+    for j in range(10):
+        g.progress(reach, 1)
+        prev_index = curr_index
+        curr_index = g.garden_index()
+        print(f'{i + 1}:{g}:{curr_index}:{curr_index - prev_index}')
+        i += 1
+
+    # at generation #122 and index 8990, the pattern starts to repeat with a delta of 58, thus
+    print(f'[12b] the plant index at generattion 50000000000 is:' +
+          f' {8990 + (50000000000 - 122 - 1) * 58}')
