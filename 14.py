27ecd87e50072202daae2cdbde2aec24d7f0a5cc
The Hungry Turnip <thehungryturnip@gmail.com>
2018-12-19 19:10:49 -0500

    Completed 14b. Not the most efficient solution.

--- 14.py
+++ 14.py
@@ -5,8 +5,8 @@
 import sys
 
 if __name__ == '__main__':
-    recipe_min = int(sys.argv[1])
-    print(f'recipe min: {recipe_min}')
+    target = sys.argv[1]
+    print(f'recipe min: {target}')
 
     recipes = [3, 7]
     index_0 = 0
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     
     def create_new_recipes(recipes, index_0, index_1):
         new_recipes = recipes[index_0] + recipes[index_1]
+        # assumes that recipe scores are between 0 and 18
         if new_recipes >= 10:
             recipes.append(1)
             new_recipes -= 10
@@ -25,10 +26,39 @@ if __name__ == '__main__':
             index -= len(recipes)
         return index
 
-    while len(recipes) < recipe_min + 10:
+    while len(recipes) < int(target) + 10:
         create_new_recipes(recipes, index_0, index_1)
         index_0 = get_next_index(recipes, index_0)
         index_1 = get_next_index(recipes, index_1)
 
-    next_10 = ''.join([str(r) for r in recipes[recipe_min:recipe_min + 10]])
-    print(f'[14a] The next 10 scroes after {recipe_min} are {next_10}')
+    next_10 = ''.join([str(r) for r in recipes[int(target):int(target) + 10]])
+    print(f'[14a] The next 10 scroes after {int(target)} are {next_10}')
+
+    recipes = [3, 7]
+    index_0 = 0
+    index_1 = 1
+    
+    # not the most efficient search:
+    # 1. in many cases could be checking once instead of twice
+    # 2. could be reusing previous search string
+    def check_2(target, recipes):
+        if len(target) > len(recipes):
+            return None
+        index_start = len(recipes) - len(target) - 1
+        index_end = index_start + len(target)
+        compare_to = ''.join([str(r) for r in recipes[index_start:index_end]])
+        if target == compare_to:
+            return index_start
+        if target == compare_to[1:] + str(recipes[index_end]):
+            return index_start + 1
+        return None
+
+    target_found_at = None
+    while not target_found_at:
+        create_new_recipes(recipes, index_0, index_1)
+        index_0 = get_next_index(recipes, index_0)
+        index_1 = get_next_index(recipes, index_1)
+        target_found_at = check_2(target, recipes)
+
+    print(f'[14b] The target {target} is found after {target_found_at}' +
+          ' recipes.')
