caa636f66c78133eedd5a6dae474b2faa47acbf7
The Hungry Turnip <thehungryturnip@gmail.com>
2019-06-07 17:42:22 -0400

    Completed day 15 part 2. Also updated pathfinding to use breath first search instead of a*.

--- 15.py
+++ 15.py
@@ -3,7 +3,7 @@
 # 2018-12-19 thehungryturnip@gmail.com
 
 import sys
-from collections import namedtuple
+from collections import deque, namedtuple 
 from enum import Enum, auto
 from heapq import heappush, heappop
 
@@ -76,10 +76,11 @@ if __name__ == '__main__':
 
         Path = namedtuple('Path', ['cost', 'coords'])
 
-        def __init__(self, rows):
+        def __init__(self, rows, elf_power):
             self.teams = {}
             self.row_count = len(rows)
             self.col_count = 0
+            self.elf_died = False
             for r, row in enumerate(rows):
                 if (len(row) > self.col_count):
                     self.col_count = len(row)
@@ -88,6 +89,8 @@ if __name__ == '__main__':
                     # Not creating pieces for empty space (i.e. '.').
                     if piece_type:
                         unit = Piece(piece_type)
+                        if piece_type == Piece.Type.ELF:
+                            unit.power = elf_power
                         self[Coord(r,c)] = unit
                         if piece_type != Piece.Type.WALL:
                             self.teams.setdefault(piece_type, {})
@@ -108,11 +111,11 @@ if __name__ == '__main__':
 
                 # Round ends without completing if no more targets.
                 if not targets_c:
-                    return False
+                    return False, self.elf_died
 
                 unit_c = self._try_move(unit_c, targets_c)
                 self._try_attack(unit_c)
-            return True
+            return True, self.elf_died
 
         def get_total_hp(self):
             return sum([self[m].hp for _, t in self.teams.items() for m in t])
@@ -138,28 +141,46 @@ if __name__ == '__main__':
                                for c in t.adjacent() 
                                if c not in self)
 
-            best_path = None
-            for c in move_targets:
-                p = self._path_between(unit_c, c)
-                if not p:
-                    continue
-                if not best_path:
-                    best_path = p
-                    continue
-                if p < best_path:
-                    best_path = p
+            # best_path = None
+            # for c in move_targets:
+            #     p = self._path_between(unit_c, c)
+            #     if not p:
+            #         continue
+            #     if not best_path:
+            #         best_path = p
+            #         continue
+            #     if p < best_path:
+            #         best_path = p
 
+            best_path = self._closest_target(unit_c, move_targets)
+            
             if not best_path:
                 return unit_c
-            
+
             # print(f'best_path:{best_path}')
             unit = self[unit_c]
-            new_coord = best_path.coords[1]
+            # new_coord = best_path.coords[1]
+            new_coord = best_path[1]
             self[new_coord] = unit
             del self[unit_c]
             self.teams[unit.type][new_coord] = None
             del self.teams[unit.type][unit_c]
             return new_coord
+
+        def _closest_target(self, c_1, move_targets):
+            in_progress = deque()
+            in_progress.append([c_1])
+            visited = set(c_1)
+            while in_progress:
+                p = in_progress.popleft()
+                for c in p[-1].adjacent():
+                    if c in visited or c in self:
+                        continue
+                    if c in move_targets:
+                        return p + [c]
+                    visited.add(c)
+                    in_progress.append(p + [c])
+            return None
 
         def _path_between(self, c_1, c_2):
             # in_progress = [Field.Path(c_1.distance_to(c_2),[c_1])]
@@ -207,6 +228,8 @@ if __name__ == '__main__':
             if not target.is_alive():
                 del self[target_c]
                 del self.teams[target.type][target_c]
+                if target.type == Piece.Type.ELF:
+                    self.elf_died = True
         
         def _str_row(self, r):
             row = ""
@@ -226,19 +249,27 @@ if __name__ == '__main__':
 
     class Battle:
 
-        def __init__(self, rows):
-            self.field = Field(rows)
+        def __init__(self, rows, elf_power=3):
+            self.field = Field(rows, elf_power)
             self.rounds = 0
 
         def process_round(self):
-            round_complete = self.field.process_round()
+            round_complete, _ = self.field.process_round()
             # if self.rounds >= 25:
             #     round_complete = False
             if round_complete:
                 self.rounds += 1
-                print(f'[{self.rounds}]')
-                print(self)
             return round_complete
+
+        def elf_dies(self):
+            round_complete = True
+            while round_complete:
+                round_complete, elf_died = self.field.process_round()
+                if round_complete:
+                    self.rounds += 1
+                if elf_died:
+                    return True
+            return False 
 
         def hp(self):
             return self.field.get_total_hp()
@@ -257,14 +288,31 @@ if __name__ == '__main__':
             line = f.readline().strip()
 
     battle = Battle(data)
+    print(f'[start!]')
     print(battle)
 
     while battle.process_round():
-        pass
-
-    print(f'[{battle.rounds}]')
+        print(f'[{battle.rounds}]')
+        print(battle)
+    print(f'[finished!]')
     print(battle)
 
     rounds = battle.rounds
     hp = battle.hp()
     print(f'[15a] Final score: {rounds} x {hp} = {rounds * hp}')
+
+    power = 4
+    battle = Battle(data, power)
+    print(f'Trying power:{power}')
+    while battle.elf_dies():
+        print(f'[elf died: {battle.rounds} rounds {battle.hp()} hp]')
+        print(battle)
+        power += 1
+        battle = Battle(data, power)
+        print(f'Trying power:{power}')
+    print(f'[victory!]')
+    print(battle)
+
+    rounds = battle.rounds
+    hp = battle.hp()
+    print(f'[15b] Final score with power {power}: {rounds} x {hp} = {rounds * hp}')
